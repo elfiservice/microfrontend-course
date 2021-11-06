282d5eae1ec4cde2b9e6e4daa6d58a78df5d7022
Armando Jr <elfiservice@hotmail.com>
2021-11-06 09:06:28 -0300

    feat : applied MFP to Products app

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,11 +1,20 @@
 __webpack_base_uri__ = 'http://localhost:8081';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports = {
   mode: 'development',
   devServer: {
     port: 8081,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'productsMFP',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './ProductsApp': './src/index',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
