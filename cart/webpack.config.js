f3650c1126909803f9e4046b9cfc60423ac227f3
Armando Jr <elfiservice@hotmail.com>
2021-11-06 09:19:15 -0300

    feat : provided the new Cart App

---
+++ cart/webpack.config.js
@@ -0,0 +1,22 @@
+__webpack_base_uri__ = 'http://localhost:8082';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8082,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'cartMFP',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './cartApp': './src/index',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
