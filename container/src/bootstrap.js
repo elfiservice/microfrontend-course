83eb1a3c15ed9c3bb3e71995c3e5ff39421895cc
Armando Jr <elfiservice@hotmail.com>
2021-11-06 09:07:30 -0300

    feat : applied MFP to Container app and made some adjusts in index.html and provided bootstrap file

---
+++ container/src/bootstrap.js
@@ -0,0 +1,4 @@
+
+import 'productsRemote/ProductsApp';
+
+console.log('Container!!');
--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,11 +1,19 @@
 __webpack_base_uri__ = 'http://localhost:8080';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
 module.exports = {
   mode: 'development',
   devServer: {
     port: 8080,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        productsRemote: 'productsMFP@http://localhost:8081/remoteEntry.js',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
